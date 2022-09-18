9ea0007cbe8aae1a1cf32a69d9eec6963e0bd5ed
Artwot <jsarabiar98@gmail.com>
2022-09-17 23:37:56 -0500

    Add the fetched data in SearchExercise component

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { Stack, Box, TextField, Button, Typography } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 const SearchExercises = () => {
-  const [searchedExercise, setSearchedExercise] = useState('');
+  const [search, setSearch] = useState('');
 
-  const handleSearch = () => {
-    console.log('Se hizo click');
+  const handleSearch = async () => {
+    if (search) {
+      const url = 'https://exercisedb.p.rapidapi.com/exercises';
+      const exercises = await fetchData(url, exerciseOptions);
+
+      // Filter the search in every category
+      const searchedExercises = exercises.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search)
+      );
+
+      console.log(searchedExercises);
+    }
   };
 
   return (
@@ -44,9 +58,9 @@ const SearchExercises = () => {
           placeholder='Buscar ejercicios'
           type='text'
           className='text-field'
-          value={searchedExercise}
+          value={search}
           variant='outlined'
-          onChange={(e) => setSearchedExercise(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)}
           sx={{ width: '75%', outline: 'none' }}
         />
         <Button
